b2048aa297977235207d96a423b2f7ca9e6f7323
andrealar30 <dreaguilar@gmail.com>
2024-06-03 22:30:38 -0600

    Agregando archivos de la primera página web

---
+++ main.js
@@ -0,0 +1,20 @@
+function changeColor(element, color) {
+    if (element.style.color === 'black') {
+        element.style.color = color;
+    } else {
+        element.style.color = 'black';
+    }
+}
+
+//creo una funcion que se ejecutara cuando hago click en el boton
+function muestra_oculta(id) {
+    //declaro la funcion y paso por parametro el selector de referencia que quiero ocultar/mostrar
+    let div = document.getElementById(id);
+    //uso el condicional IF para saber si el DIV esta oculto (Display: none) o si esta visible 
+    if (div.style.display == "none"){ 
+      div.style.display = "flex";
+    } 
+    else {
+      div.style.display = "none";
+    }
+}
